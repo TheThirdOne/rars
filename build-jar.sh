cb96a1b85434bde6366df7574dcaef93f0e5f0af
Jgbcode <Jgbcode@gmail.com>
2019-06-01 23:52:27 -0400

    Updated build-jar.sh
    Added copied License.txt to src/main/resources so it gets automatically
    bundled in the build. Modified build-jar.sh to add README.md to the
    build.

--- build-jar.sh
+++ build-jar.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 mkdir -p build
-find ./src/main/java/rars -name "*.java" | xargs javac -d build 
+find ./src/main/java/rars -name "*.java" | xargs javac -d build
+
+# Copy resources to jar
 cp -r ./src/main/resources/* build
+
+# Copy README.md to jar
+cp README.md screenshot.png build
+
 cd build
 jar cfm ../rars.jar ../src/main/resources/META-INF/MANIFEST.MF *
