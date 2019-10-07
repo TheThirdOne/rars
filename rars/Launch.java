TheThirdOne/rars

commit 07581f9e5e733f0e41cfbf0396466dd1af44b3dc

Add stable Java API for running programs
The goal of this is to make automated testing much faster because the
JVM doesn't have to start up a bunch.

rars/Launch.java
@@ -1,5 +1,6 @@
 package rars;
 
+import rars.api.Program;
 import rars.riscv.dump.DumpFormat;
 import rars.riscv.dump.DumpFormatLoader;
 import rars.riscv.hardware.*;
@@ -9,6 +10,7 @@ import rars.util.Binary;
 import rars.util.FilenameFinder;
 import rars.util.MemoryDump;
 import rars.venus.VenusUI;
+import rars.api.Options;
 
 import javax.swing.*;
 import java.io.File;
@@ -105,16 +107,12 @@ public class Launch {
      * made available to the program at runtime.<br>
      **/
 
-
+    private Options options;
     private boolean simulate;
     private int displayFormat;
     private boolean verbose;  // display register name or address along with contents
     private boolean assembleProject; // assemble only the given file or all files in its directory
-    private boolean pseudo;  // pseudo instructions allowed in source code or not.
-    private boolean warningsAreErrors; // Whether assembler warnings should be considered errors.
-    private boolean startAtMain; // Whether to start execution at statement labeled 'main'
     private boolean countInstructions; // Whether to count and report number of instructions executed
-    private boolean selfModifyingCode; // Whether to allow self-modifying code (e.g. write to text segment)
     private static final String rangeSeparator = "-";
     private static final int memoryWordsPerLine = 4; // display 4 memory words, tab separated, per line
     private static final int DECIMAL = 0; // memory and register display format
@@ -123,8 +121,6 @@ public class Launch {
     private ArrayList<String> registerDisplayList;
     private ArrayList<String> memoryDisplayList;
     private ArrayList<String> filenameList;
-    private RISCVprogram code;
-    private int maxSteps;
     private int instructionCount;
     private PrintStream out; // stream for display of command line output
     private ArrayList<String[]> dumpTriples = null; // each element holds 3 arguments for dump option
@@ -143,15 +139,12 @@ public class Launch {
         } else { // running from command line.
             // assure command mode works in headless environment (generates exception if not)
             System.setProperty("java.awt.headless", "true");
+            options = new Options();
             simulate = true;
             displayFormat = HEXADECIMAL;
             verbose = true;
             assembleProject = false;
-            pseudo = true;
-            warningsAreErrors = false;
-            startAtMain = false;
             countInstructions = false;
-            selfModifyingCode = false;
             instructionCount = 0;
             assembleErrorExitCode = 0;
             simulateErrorExitCode = 0;
@@ -159,9 +152,6 @@ public class Launch {
             memoryDisplayList = new ArrayList<>();
             filenameList = new ArrayList<>();
             MemoryConfigurations.setCurrentConfiguration(MemoryConfigurations.getDefaultConfiguration());
-            // do NOT use Globals.program for command line RARS -- it triggers 'backstep' log.
-            code = new RISCVprogram();
-            maxSteps = -1;
             out = System.out;
             if (parseCommandArgs(args)) {
                 runCommand();
@@ -371,19 +361,19 @@ public class Launch {
                 continue;
             }
             if (args[i].toLowerCase().equals("np") || args[i].toLowerCase().equals("ne")) {
-                pseudo = false;
+                options.pseudo = false;
                 continue;
             }
             if (args[i].toLowerCase().equals("we")) { // added 14-July-2008 DPS
-                warningsAreErrors = true;
+                options.warningsAreErrors = true;
                 continue;
             }
             if (args[i].toLowerCase().equals("sm")) { // added 17-Dec-2009 DPS
-                startAtMain = true;
+                options.startAtMain = true;
                 continue;
             }
             if (args[i].toLowerCase().equals("smc")) { // added 5-Jul-2013 DPS
-                selfModifyingCode = true;
+                options.selfModifyingCode = true;
                 continue;
             }
             if (args[i].toLowerCase().equals("ic")) { // added 19-Jul-2012 DPS
@@ -414,7 +404,7 @@ public class Launch {
             // Check for stand-alone integer, which is the max execution steps option
             try {
                 Integer.decode(args[i]);
-                maxSteps = Integer.decode(args[i]); // if we got here, it has to be OK
+                options.maxSteps = Integer.decode(args[i]); // if we got here, it has to be OK
                 continue;
             } catch (NumberFormatException nfe) {
             }
@@ -447,7 +437,6 @@ public class Launch {
             return;
         }
 
-        Globals.getSettings().setBooleanSettingNonPersistent(Settings.Bool.SELF_MODIFYING_CODE_ENABLED, selfModifyingCode);
         File mainFile = new File(filenameList.get(0)).getAbsoluteFile();// First file is "main" file
         ArrayList<String> filesToAssemble;
         if (assembleProject) {
@@ -472,17 +461,12 @@ public class Launch {
         } else {
             filesToAssemble = FilenameFinder.getFilenameList(filenameList, FilenameFinder.MATCH_ALL_EXTENSIONS);
         }
+        Program program = new Program(options);
         try {
             if (Globals.debug) {
-                out.println("--------  TOKENIZING BEGINS  -----------");
+                out.println("---  TOKENIZING & ASSEMBLY BEGINS  ---");
             }
-            ArrayList<RISCVprogram> programsToAssemble =
-                    code.prepareFilesForAssembly(filesToAssemble, mainFile.getAbsolutePath(), null);
-            if (Globals.debug) {
-                out.println("--------  ASSEMBLY BEGINS  -----------");
-            }
-            // Added logic to check for warnings and print if any. DPS 11/28/06
-            ErrorList warnings = code.assemble(programsToAssemble, pseudo, warningsAreErrors);
+            ErrorList warnings = program.assemble(filesToAssemble, mainFile.getAbsolutePath());
             if (warnings != null && warnings.warningsOccurred()) {
                 out.println(warnings.generateWarningReport());
             }
@@ -493,10 +477,7 @@ public class Launch {
             return;
         }
         if (simulate) {
-            RegisterFile.initializeProgramCounter(startAtMain); // DPS 3/9/09
-
-            // store program args (if any) in memory
-            new ProgramArgumentList(programArgumentList).storeProgramArguments();
+            program.setup(programArgumentList,null);
             // establish observer if specified
             establishObserver();
             if (Globals.debug) {
@@ -504,9 +485,9 @@ public class Launch {
             }
             try {
                 while (true) {
-                    Simulator.Reason done = code.simulate(maxSteps);
+                    Simulator.Reason done = program.simulate();
                     if (done == Simulator.Reason.MAX_STEPS) {
-                        out.println("\nProgram terminated when maximum step limit " + maxSteps + " reached.");
+                        out.println("\nProgram terminated when maximum step limit " + options.maxSteps + " reached.");
                         break;
                     } else if (done == Simulator.Reason.CLIFF_TERMINATION) {
                         out.println("\nProgram terminated by dropping off the bottom.");

rars/RISCVprogram.java
@@ -8,6 +8,7 @@ import rars.simulator.Simulator;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
 Copyright (c) 2003-2006,  Pete Sanderson and Kenneth Vollmar
@@ -210,6 +211,16 @@ public class RISCVprogram {
             return null;
     }
 
+
+    /**
+     * Reads RISCV source code from a string into structure.
+     *
+     * @param source String containing the RISCV source code.
+     **/
+    public void fromString(String source){
+        this.filename = source;
+        this.sourceList = new ArrayList<>(Arrays.asList(source.split( "\n")));
+    }
 
     /**
      * Reads RISCV source code from file into structure.  Will always read from file.

rars/api/Options.java
@@ -1,0 +1,16 @@
+package rars.api;
+
+public class Options {
+    public boolean pseudo;            // pseudo instructions allowed in source code or not.
+    public boolean warningsAreErrors; // Whether assembler warnings should be considered errors.
+    public boolean startAtMain;       // Whether to start execution at statement labeled 'main'
+    public boolean selfModifyingCode; // Whether to allow self-modifying code (e.g. write to text segment)
+    public int maxSteps;
+    public Options(){
+        pseudo = true;
+        warningsAreErrors = false;
+        startAtMain = false;
+        selfModifyingCode = false;
+        maxSteps = -1;
+    }
+}

rars/api/Program.java
@@ -1,0 +1,258 @@
+package rars.api;
+
+import rars.*;
+import rars.riscv.hardware.*;
+import rars.simulator.ProgramArgumentList;
+import rars.simulator.Simulator;
+import rars.util.SystemIO;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+/**
+ * <p>
+ * This is most of the public API for running RARS programs. It wraps internal
+ * APIs to provide a base for making applications to simulate many programs.
+ * </p>
+ *
+ * The order you are expected to run the methods is:
+ * <ol>
+ * <li> assemble(...)
+ * <li> setup(...)
+ * <li> get/set for any specific setup
+ * <li> simulate()
+ * <li> get/set to check output
+ * <li> repeat 3-5 if simulation hasn't terminated
+ * <li> repeat 2-6 for multiple inputs as needed
+ * </ol>
+ *
+ * <p>
+ * Importantly, only one instance of Program can be setup at a time (this may
+ * change in the future). Only the most recent program to be setup is valid to
+ * call simulate on. Additionally, reading registers or memory is also only valid
+ * once setup has been called and before another setup is called.
+ * </p>
+ *
+ * <p>
+ * Also, it is not threadsafe, calling assemble in another thread could invalidate
+ * a concurrent simulation.
+ * </p>
+ */
+public class Program {
+
+    private Options set;
+    private RISCVprogram code;
+    private SystemIO.Data fds;
+    private ByteArrayOutputStream stdout, stderr;
+    private Memory assembled, simulation;
+    private int startPC, exitCode;
+
+    public Program() {
+        this(new Options());
+    }
+
+    public Program(Options set){
+        Globals.initialize(false);
+        this.set = set;
+        code = new RISCVprogram();
+        assembled = new Memory();
+        simulation = new Memory();
+    }
+
+    /**
+     * Assembles from a list of files
+     *
+     * @param files A list of files to assemble
+     * @param main Which file should be considered the main file; it should be in files
+     * @return A list of warnings generated if Options.warningsAreErrors is true, this will be empty
+     * @throws AssemblyException thrown if any errors are found in the code
+     */
+    public ErrorList assemble(ArrayList<String> files, String main) throws AssemblyException {
+        ArrayList<RISCVprogram> programs = code.prepareFilesForAssembly(files,main, null);
+        return assemble(programs);
+    }
+
+    /**
+     * Assembles a single file
+     *
+     * @param file path to the file to assemble
+     * @return A list of warnings generated if Options.warningsAreErrors is true, this will be empty
+     * @throws AssemblyException thrown if any errors are found in the code
+     */
+    public ErrorList assemble(String file) throws AssemblyException {
+        // TODO: potentially inline prepareForAssembly
+        ArrayList<String> files = new ArrayList<>();
+        files.add(file);
+        ArrayList<RISCVprogram> programs = code.prepareFilesForAssembly(files,file, null);
+        return assemble(programs);
+    }
+
+    /**
+     * Assembles a string as RISC-V source code
+     *
+     * @param source the code to assemble
+     * @return A list of warnings generated if Options.warningsAreErrors is true, this will be empty
+     * @throws AssemblyException thrown if any errors are found in the code
+     */
+    public ErrorList assembleString(String source) throws AssemblyException {
+        ArrayList<RISCVprogram> programs = new ArrayList<>();
+        code.fromString(source);
+        code.tokenize();
+        programs.add(code);
+        return assemble(programs);
+    }
+
+    private ErrorList assemble(ArrayList<RISCVprogram> programs) throws AssemblyException {
+        Memory temp = Memory.swapInstance(assembled); // Assembling changes memory so we need to swap to capture that.
+        ErrorList warnings = null;
+        AssemblyException e = null;
+        try {
+            warnings = code.assemble(programs,set.pseudo,set.warningsAreErrors);
+        }catch(AssemblyException ae){
+            e = ae;
+        }
+        Memory.swapInstance(temp);
+        if(e != null)throw e;
+
+        RegisterFile.initializeProgramCounter(set.startAtMain);
+        startPC = RegisterFile.getProgramCounter();
+
+        return warnings;
+    }
+
+    /**
+     * Prepares the simulator for execution. Clears registers, loads arguments
+     * into memory and initializes the String backed STDIO
+     *
+     * @param args Just like the args to a Java main, but an ArrayList.
+     * @param STDIN A string that can be read in the program like its stdin or null to allow IO passthrough
+     */
+    public void setup(ArrayList<String> args, String STDIN){
+        RegisterFile.resetRegisters();
+        FloatingPointRegisterFile.resetRegisters();
+        ControlAndStatusRegisterFile.resetRegisters();
+        InterruptController.reset();
+        RegisterFile.initializeProgramCounter(startPC);
+        Globals.exitCode = 0;
+
+        // Copy in assembled code and arguments
+        simulation.copyFrom(assembled);
+        Memory tmpMem = Memory.swapInstance(simulation);
+        new ProgramArgumentList(args).storeProgramArguments();
+        Memory.swapInstance(tmpMem);
+
+        // To capture the IO we need to replace stdin and friends
+        fds = new SystemIO.Data(true);
+        if(STDIN != null) {
+            fds.streams[0] = new ByteArrayInputStream(STDIN.getBytes());
+            fds.streams[1] = stdout = new ByteArrayOutputStream();
+            fds.streams[2] = stderr = new ByteArrayOutputStream();
+        }
+    }
+
+    /**
+     * Simulates a processor executing the machine code.
+     *
+     * @return the reason why simulation was paused or terminated.
+     *         Possible values are: <ul>
+     *              <li> BREAKPOINT (caused by ebreak instruction),
+     *              <li> MAX_STEPS (caused by simulating Options.maxSteps instructions),
+     *              <li> NORMAL_TERMINATION (caused by executing the exit system call)
+     *              <li> CLIFF_TERMINATION (caused by the program overflowing the written code). </ul>
+     *         Only BREAKPOINT and MAX_STEPS can be simulated further.
+     * @throws SimulationException thrown if there is an uncaught interrupt. The program cannot be simulated further.
+     */
+    public Simulator.Reason simulate() throws SimulationException {
+        Simulator.Reason ret = null;
+        SimulationException e = null;
+
+        // Swap out global state for local state.
+        boolean selfMod = Globals.getSettings().getBooleanSetting(Settings.Bool.SELF_MODIFYING_CODE_ENABLED);
+        Globals.getSettings().setBooleanSettingNonPersistent(Settings.Bool.SELF_MODIFYING_CODE_ENABLED, set.selfModifyingCode);
+        SystemIO.Data tmpFiles = SystemIO.swapData(fds);
+        Memory tmpMem = Memory.swapInstance(simulation);
+
+        try {
+            ret = code.simulate(set.maxSteps);
+        }catch(SimulationException se){
+            e = se;
+        }
+        exitCode = Globals.exitCode;
+
+        Globals.getSettings().setBooleanSettingNonPersistent(Settings.Bool.SELF_MODIFYING_CODE_ENABLED, selfMod);
+        SystemIO.swapData(tmpFiles);
+        Memory.swapInstance(tmpMem);
+
+        if(e != null)throw e;
+        return ret;
+    }
+
+    /**
+     * @return converts the bytes sent to stdout into a string (resets to "" when setup is called)
+     */
+    public String getSTDOUT(){
+        return stdout.toString();
+    }
+
+    /**
+     * @return converts the bytes sent to stderr into a string (resets to "" when setup is called)
+     */
+    public String getSTDERR(){
+        return stderr.toString();
+    }
+
+    /**
+     * Gets the value of a normal, floating-point or control and status register.
+     *
+     * @param name Either the common usage (t0, a0, ft0), explicit numbering (x2, x3, f0), or CSR name (ustatus)
+     * @return The value of the register as an int (floats are encoded as IEEE-754)
+     * @throws NullPointerException if name is invalid; only needs to be checked if code accesses arbitrary names
+     */
+    public int getRegisterValue(String name){
+        Register r = RegisterFile.getRegister(name);
+        if(r == null){
+            r = FloatingPointRegisterFile.getRegister(name);
+        }
+        if(r == null){
+            return ControlAndStatusRegisterFile.getValue(name);
+        }else{
+            return r.getValue();
+        }
+    }
+
+    /**
+     * Gets the value of a normal, floating-point or control and status register.
+     *
+     * @param name Either the common usage (t0, a0, ft0), explicit numbering (x2, x3, f0), or CSR name (ustatus)
+     * @param value The value of the register as an int (floats are encoded as IEEE-754)
+     * @throws NullPointerException if name is invalid; only needs to be checked if code accesses arbitrary names
+     */
+    public void setRegisterValue(String name, int value){
+        Register r = RegisterFile.getRegister(name);
+        if(r == null){
+            r = FloatingPointRegisterFile.getRegister(name);
+        }
+        if(r == null){
+            ControlAndStatusRegisterFile.updateRegister(name,value);
+        }else{
+            r.setValue(value);
+        }
+    }
+
+    /**
+     * Returns the exit code passed to the exit syscall if it was called, otherwise returns 0
+     */
+    public int getExitCode(){
+        return exitCode;
+    }
+
+    /**
+     * Gets the instance of memory the program is using.
+     *
+     * This is only valid when setup has been called.
+     */
+    public Memory getMemory(){
+        return simulation;
+    }
+}

commit 0703e794abcae721bc6c56185e8f010ce6a1a317

Fix bugs related to command line interface
ic, register logging, memory logging and dump sections were all broken by
07581f9e5e733f0e41cfbf0396466dd1af44b3dc . The global state for the program
is now held inside of a program object instead of globally accessible, so
logging gets the zeroed memoery and registers rather than the chnaged ones.

I noticed this while working on documentation for the commandline. The fix is
to pass the program object into the logging functions so they can use local
references to the data rather than reference Globals.memory or RegisterFile.

Dump has not been fixed yet as that is going to touch more files.

Obviously #13 is related, and this also shows how to get lines executed as part
of the api. A bonus of this is that it removes the jank way ic used to be calculated.

rars/Launch.java
@@ -154,25 +154,24 @@ public class Launch {
             MemoryConfigurations.setCurrentConfiguration(MemoryConfigurations.getDefaultConfiguration());
             out = System.out;
             if (parseCommandArgs(args)) {
-                runCommand();
-                dumpSegments();
+                dumpSegments(runCommand());
             }
             System.exit(Globals.exitCode);
         }
     }
 
-    private void displayAllPostMortem() {
-        displayMiscellaneousPostMortem();
-        displayRegistersPostMortem();
-        displayMemoryPostMortem();
+    private void displayAllPostMortem(Program program) {
+        displayMiscellaneousPostMortem(program);
+        displayRegistersPostMortem(program);
+        displayMemoryPostMortem(program.getMemory());
     }
     /////////////////////////////////////////////////////////////
     // Perform any specified dump operations.  See "dump" option.
     //
 
-    private void dumpSegments() {
-
-        if (dumpTriples == null)
+    private void dumpSegments(Program program) {
+        // TODO: make dump segments work on api.Program
+        if (dumpTriples == null || program == null)
             return;
 
         for (String[] triple : dumpTriples) {
@@ -432,9 +431,9 @@ public class Launch {
     // Carry out the rars command: assemble then optionally run
     // Returns false if no simulation (run) occurs, true otherwise.
 
-    private void runCommand() {
+    private Program runCommand() {
         if (filenameList.size() == 0) {
-            return;
+            return null;
         }
 
         File mainFile = new File(filenameList.get(0)).getAbsoluteFile();// First file is "main" file
@@ -474,12 +473,10 @@ public class Launch {
             Globals.exitCode = assembleErrorExitCode;
             out.println(e.errors().generateErrorAndWarningReport());
             out.println("Processing terminated due to errors.");
-            return;
+            return null;
         }
         if (simulate) {
             program.setup(programArgumentList,null);
-            // establish observer if specified
-            establishObserver();
             if (Globals.debug) {
                 out.println("--------  SIMULATION BEGINS  -----------");
             }
@@ -497,7 +494,7 @@ public class Launch {
                         break;
                     }
                     assert done == Simulator.Reason.BREAKPOINT : "Internal error: All cases other than breakpoints should be handled already";
-                    displayAllPostMortem(); // print registers if we hit a breakpoint, then continue
+                    displayAllPostMortem(program); // print registers if we hit a breakpoint, then continue
                 }
 
             } catch (SimulationException e) {
@@ -505,11 +502,12 @@ public class Launch {
                 out.println(e.error().generateReport());
                 out.println("Simulation terminated due to errors.");
             }
-            displayAllPostMortem();
+            displayAllPostMortem(program);
         }
         if (Globals.debug) {
             out.println("\n--------  ALL PROCESSING COMPLETE  -----------");
         }
+        return program;
     }
 
 
@@ -538,45 +536,12 @@ public class Launch {
         return memoryRange;
     }
 
-    /////////////////////////////////////////////////////////////////
-    // Required for counting instructions executed, if that option is specified.
-    // DPS 19 July 2012
-    private void establishObserver() {
-        if (countInstructions) {
-            Observer instructionCounter =
-                    new Observer() {
-                        private int lastAddress = 0;
-
-                        public void update(Observable o, Object obj) {
-                            if (obj instanceof AccessNotice) {
-                                AccessNotice notice = (AccessNotice) obj;
-                                if (!notice.accessIsFromRISCV())
-                                    return;
-                                if (notice.getAccessType() != AccessNotice.READ)
-                                    return;
-                                MemoryAccessNotice m = (MemoryAccessNotice) notice;
-                                int a = m.getAddress();
-                                if (a == lastAddress)
-                                    return;
-                                lastAddress = a;
-                                instructionCount++;
-                            }
-                        }
-                    };
-            try {
-                Globals.memory.addObserver(instructionCounter, Memory.textBaseAddress, Memory.textLimitAddress);
-            } catch (AddressErrorException aee) {
-                out.println("Internal error: Launch uses incorrect text segment address for instruction observer");
-            }
-        }
-    }
-
     //////////////////////////////////////////////////////////////////////
     // Displays any specified runtime properties. Initially just instruction count
     // DPS 19 July 2012
-    private void displayMiscellaneousPostMortem() {
+    private void displayMiscellaneousPostMortem(Program program) {
         if (countInstructions) {
-            out.println("\n" + instructionCount);
+            out.println("\n" + program.getRegisterValue("cycle"));
         }
     }
 
@@ -584,26 +549,19 @@ public class Launch {
     //////////////////////////////////////////////////////////////////////
     // Displays requested register or registers
 
-    private void displayRegistersPostMortem() {
+    private void displayRegistersPostMortem(Program program) {
         // Display requested register contents
         for (String reg : registerDisplayList) {
-            if (RegisterFile.getRegister(reg) != null) {
-                // integer register
-                if (verbose)
-                    out.print(reg + "\t");
-                out.println(formatIntForDisplay(RegisterFile.getRegister(reg).getValue()));
-            } else {
+            if(FloatingPointRegisterFile.getRegister(reg) != null){
                 // floating point register
-                float fvalue = FloatingPointRegisterFile.getFloatFromRegister(reg);
-                int ivalue = FloatingPointRegisterFile.getValue(reg);
+                int ivalue = program.getRegisterValue(reg);
+                float fvalue = Float.intBitsToFloat(ivalue);
                 if (verbose) {
                     out.print(reg + "\t");
                 }
                 if (displayFormat == HEXADECIMAL) {
                     // display float (and double, if applicable) in hex
-                    out.println(
-                            Binary.binaryStringToHexString(
-                                    Binary.intToBinaryString(ivalue)));
+                    out.println(Binary.intToHexString(ivalue));
 
                 } else if (displayFormat == DECIMAL) {
                     // display float (and double, if applicable) in decimal
@@ -612,6 +570,10 @@ public class Launch {
                 } else { // displayFormat == ASCII
                     out.println(Binary.intToAscii(ivalue));
                 }
+            } else { // Integer register or CSR
+                if (verbose)
+                    out.print(reg + "\t");
+                out.println(formatIntForDisplay(RegisterFile.getRegister(reg).getValue()));
             }
         }
     }
@@ -639,7 +601,7 @@ public class Launch {
     //////////////////////////////////////////////////////////////////////
     // Displays requested memory range or ranges
 
-    private void displayMemoryPostMortem() {
+    private void displayMemoryPostMortem(Memory memory) {
         int value;
         // Display requested memory range contents
         Iterator<String> memIter = memoryDisplayList.iterator();
@@ -663,10 +625,10 @@ public class Launch {
                 try {
                     // Allow display of binary text segment (machine code) DPS 14-July-2008
                     if (Memory.inTextSegment(addr)) {
-                        Integer iValue = Globals.memory.getRawWordOrNull(addr);
+                        Integer iValue = memory.getRawWordOrNull(addr);
                         value = (iValue == null) ? 0 : iValue;
                     } else {
-                        value = Globals.memory.getWord(addr);
+                        value = memory.getWord(addr);
                     }
                     out.print(formatIntForDisplay(value) + "\t");
                 } catch (AddressErrorException aee) {