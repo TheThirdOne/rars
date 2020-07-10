b3afbbe591e268caa37bf007fac4e919ce0ca48a
Benjamin Landers <benjaminrlanders@gmail.com>
2020-07-10 16:27:02 -0700

    Fix missing register in syscall description

--- src/rars/riscv/syscalls/SyscallTime.java
+++ src/rars/riscv/syscalls/SyscallTime.java
@@ -35,7 +35,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 public class SyscallTime extends AbstractSyscall {
     public SyscallTime() {
-        super("Time", "Get the current time (milliseconds since 1 January 1970)", "N/A", "a0 = low order 32 bits<br>high order 32 bits");
+        super("Time", "Get the current time (milliseconds since 1 January 1970)", "N/A", "a0 = low order 32 bits<br>a1=high order 32 bits");
     }
 
     public void simulate(ProgramStatement statement) {
