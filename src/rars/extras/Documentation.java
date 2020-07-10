2faba4ce1a6c404d9b97b521efa2ffc3125cfaab
Benjamin Landers <benjaminrlanders@gmail.com>
2020-07-10 14:30:56 -0700

    Add separate 64 bit instruction table to documentation

--- src/rars/extras/Documentation.java
+++ src/rars/extras/Documentation.java
@@ -1,12 +1,14 @@
 package rars.extras;
 
 import rars.Globals;
+import rars.Settings;
 import rars.assembler.Directives;
 import rars.riscv.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 
 /**
  * Small class for automatically generating documentation.
@@ -23,6 +25,8 @@ public class Documentation {
         System.out.println(createSyscallMarkdown());
         System.out.println(createInstructionMarkdown(BasicInstruction.class));
         System.out.println(createInstructionMarkdown(ExtendedInstruction.class));
+        System.out.println(createInstructionMarkdown64Only(BasicInstruction.class));
+        System.out.println(createInstructionMarkdown64Only(ExtendedInstruction.class));
     }
 
     public static String createDirectiveMarkdown(){
@@ -61,11 +65,45 @@ public class Documentation {
     }
 
     public static String createInstructionMarkdown(Class<? extends Instruction> instructionClass){
+        Globals.getSettings().setBooleanSettingNonPersistent(Settings.Bool.RV64_ENABLED,false);
+        InstructionSet.rv64 = false;
+        Globals.instructionSet.populate();
+
         ArrayList<Instruction> instructionList = Globals.instructionSet.getInstructionList();
         instructionList.sort(Comparator.comparing(Instruction::getExampleFormat));
+
         StringBuilder output = new StringBuilder("| Example Usage | Description |\n|---------------|-------------|");
         for (Instruction instr : instructionList) {
             if (instructionClass.isInstance(instr)) {
+                output.append("\n|");
+                output.append(instr.getExampleFormat());
+                output.append('|');
+                output.append(instr.getDescription());
+                output.append('|');
+            }
+        }
+        return output.toString();
+    }
+    public static String createInstructionMarkdown64Only(Class<? extends Instruction> instructionClass){
+
+        Globals.getSettings().setBooleanSettingNonPersistent(Settings.Bool.RV64_ENABLED,false);
+        InstructionSet.rv64 = false;
+        Globals.instructionSet.populate();
+
+        HashSet<String> set = new HashSet<>();
+        for (Instruction i : Globals.instructionSet.getInstructionList()){
+            set.add(i.getExampleFormat());
+        }
+
+        Globals.getSettings().setBooleanSettingNonPersistent(Settings.Bool.RV64_ENABLED,true);
+        InstructionSet.rv64 = true;
+        Globals.instructionSet.populate();
+
+        ArrayList<Instruction> instructionList64 = Globals.instructionSet.getInstructionList();
+        instructionList64.sort(Comparator.comparing(Instruction::getExampleFormat));
+        StringBuilder output = new StringBuilder("| Example Usage | Description |\n|---------------|-------------|");
+        for (Instruction instr : instructionList64) {
+            if (instructionClass.isInstance(instr) && !set.contains(instr.getExampleFormat())) {
                 output.append("\n|");
                 output.append(instr.getExampleFormat());
                 output.append('|');
