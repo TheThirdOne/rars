d37dcf555e8564077b8f6f840cbc147f254550cc
Siva Chowdeswar Nandipati <Nandipatisivachowdeswar@gmail.com>
2021-05-27 23:20:51 +0530

    General Reserve Table 27 May

--- src/rars/riscv/hardware/ReserveTable.java
+++ src/rars/riscv/hardware/ReserveTable.java
@@ -29,48 +29,33 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 public class ReserveTable{
 	//0 represents Stored value;;
-	public static ArrayList<ArrayList> arrrayProc = new ArrayList<ArrayList>();
+	public static ArrayList<ArrayList> arrrayProc = new ArrayList<ArrayList>();//Reserve Table for 
 	public static int procs = 0;
+	public static ArrayList<Integer> oldestReserve = new ArrayList<Integer>()
 	public static void ResetReserve(){
 		arrayProc = new ArrayList<ArrayList>(); 
 	}
 
 	public static void addproc(int address, int processor){
-		arrrayProc.get(processor).get(1).add(Integer.valueOf(address));
-	}
 
-	public static void addbool(int address){
-		arrrayProc.get(processor).get(0).add(Integer.valueOf(address));
+		arrrayProc.get(processor).add(Integer.valueOf(address));
 	}
 
 	public static void lr_w(int address, int processor){
-		if(arrrayProc.get(processor).get(1).contains(Integer.valueOf(address)))
+		if(arrrayProc.get(processor).contains(Integer.valueOf(address)))
 			return;
 		addproc(address, processor);
 	}
 
-	private static void addToRest(int address, int processor){
-		if(procs = 0)
-			return;
-		for(int i = 0; i <= processor; i++){
-			if(i == processor)
-				continue;
-			if(arrrayProc.get(i).get(1).contains(address)){
-				arrrayProc.get(i).get(0).add(address)
-			}
-		}
-	}
-
 	public static boolean sc_w(int address, int processor){
-		if(!arrrayProc.get(processor).get(1).contains(Integer.valueOf(address))){
-			return false; // -------------------------------------------------> Doubt Doubt Doubt Doubt Doubt Doubt Doubt Doubt 
-		}
-		if(arrrayProc.get(processor).get(0).contains(Integer.valueOf(address))){
-			arrrayProc.get(processor).get(1).remove(Integer.valueOf(address));
-			arrrayProc.get(processor).get(0).remove(Integer.valueOf(address));
+		if(!arrrayProc.get(processor).contains(Integer.valueOf(address))){
 			return false;
 		}
-		addToRest(address, processor);
+		for(int i = 0; i < procs; i++){
+			if(arrrayProc.get(i).contains(address)){
+				arrrayProc.remove(Integer.valueOf(address));
+			}
+		}
 		return true;
 	}
 	public static setProcs(int procces){
