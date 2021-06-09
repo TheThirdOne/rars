095b7158517a2fabc70eac0466a374a9281692f4
giancarlopernudisegura <pernudig@gmail.com>
2021-06-09 13:08:54 -0600

    update getInstructionCategory description for instruction support

--- src/rars/tools/InstructionStatistics.java
+++ src/rars/tools/InstructionStatistics.java
@@ -226,7 +226,7 @@ public class InstructionStatistics extends AbstractToolAndApplication {
      * decodes the instruction and determines the category of the instruction.
      * <p>
      * The instruction is decoded by checking the java instance of the instruction.
-     * Only the most relevant instructions are decoded and categorized.
+     * Supported instructions are RV32I, RV64I, M, and A extensions.
      *
      * @param instruction the instruction to decode
      * @return the category of the instruction
