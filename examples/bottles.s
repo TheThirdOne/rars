71242a53c4cd396ebf62a35af1f75ee73b727f28
Filippo Piazza <filippopiazza.02@gmail.com>
2022-09-22 10:17:01 +0200

    Fixed code
    Former code called a printf function and wouldn't work.
    New code has a internal print functions, works as intended.
    
    Consider removing save state and load state code, if not there as an example, as it's not needed to run the code.

--- examples/bottles.s
+++ examples/bottles.s
@@ -1,8 +1,12 @@
 
 .globl  main
 .data
-template:
-	.string "%d bottles of beer of the wall. %d bottles of beer. Take one down pass it around. %d bottles of beer on the wall\n"
+templ1:
+	.string " bottles of beer of the wall. "
+templ2:
+	.string " bottles of beer. Take one down pass it around. "
+templ3:	
+	.string " bottles of beer on the wall\n"
 
 .text
 main:
@@ -12,16 +16,18 @@ main:
         sw      s0,8(sp)
         sw      ra,12(sp)
 
-
-	# Call printf(template, x, x, x-1) for 0 < x < 100 
+	# load strings and int(99) for the loop
         li      s0,99
+        la      s6, templ1 #templ1 address
+        la	s7, templ2 #templ2 address
+        la	s8, templ3 #templ3 address
 loop:
         mv      a1,s0
-	mv	a2,s0
 	addi	s0,s0, -1
         mv      a3,s0
-        la      a0, template
-        call    printf
+        mv	s9, a1     #bottle
+        mv	s10, a3    #bottle-1
+        jal print
         bnez    s0,loop
 
 	# Load state
@@ -30,10 +36,35 @@ loop:
         lw      s1,4(sp)
         addi     sp,sp,16
         
-	# Exit (93) with code 0
+exit:	# Exit (93) with code 0
         li a0, 0
         li a7, 93
         ecall
         ebreak
         
-      
+print:	#Set print mode, load stuff to print, print
+	li	a7, 1 #num
+	mv	a0, s9
+	ecall
+	
+	li	a7, 4 #templ1
+	mv	a0, s6
+	ecall
+	
+	li	a7, 1 #num
+	mv	a0, s9
+	ecall
+	
+	li	a7, 4 #templ2
+	mv	a0, s7
+	ecall
+
+	li	a7, 1 #num
+	mv	a0, s10
+	ecall
+		
+	li	a7, 4 #templ3
+	mv	a0, s8
+	ecall	
+	
+	ret
