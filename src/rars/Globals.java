a59d90c9711e6dfde22b6fe7cb1acff9f5b91b79
Mattia Mascarello <mattia.mascarello@outlook.it>
2023-03-20 15:27:39 +0100

    Some modesty, found the right file

--- src/rars/Globals.java
+++ src/rars/Globals.java
@@ -145,7 +145,7 @@ public class Globals {
     }
 
     private static String getCopyrightHolders() {
-        return "Pete Sanderson, Kenneth Vollmar, modern ui fork by Mattia Mascarello";
+        return "Pete Sanderson, Kenneth Vollmar";
     }
 
     public static void setGui(VenusUI g) {
