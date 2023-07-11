37b937b98f080937ab005e217b0769a0245277e6
Daniel Blažek <blazeda5@fit.cvut.cz>
2023-07-11 22:44:18 +0200

    Use UTF-8 encoding in test.sh, fix test.sh for mingw and cygwin

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-javac -cp rars.jar test/Test.java
-java -cp test:rars.jar Test
+javac -encoding utf-8 -cp rars.jar test/Test.java
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+	java -Dfile.encoding=UTF8 -cp "test;rars.jar" Test
+else
+	java -Dfile.encoding=UTF8 -cp test:rars.jar Test
+fi
 rm test/Test.class
