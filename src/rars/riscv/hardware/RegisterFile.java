1f442a193c7067bd334555b27ddbc607afa6cf8d
Jean Privat <jean@pryen.org>
2023-08-18 09:02:42 -0400

    add RegisterFile.getValueLong(String) like the non-Long method

--- src/rars/riscv/hardware/RegisterFile.java
+++ src/rars/riscv/hardware/RegisterFile.java
@@ -119,7 +119,10 @@ public class RegisterFile {
 
     public static long getValueLong(int num) {
         return instance.getValue(num);
+    }
 
+    public static long getValueLong(String name) {
+        return instance.getValue(name);
     }
 
     /**
